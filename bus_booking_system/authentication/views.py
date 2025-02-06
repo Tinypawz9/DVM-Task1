1748279d2097fea91fac0a2cfa393372860a278b
Tinypawz9 <arjuncoder27@gmail.com>
2025-02-06 23:55:52 +0530

    Modified login view to separate admin login and passenger login.

--- bus_booking_system/authentication/views.py
+++ bus_booking_system/authentication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 
 def home(request):
@@ -13,9 +14,28 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('home_page.html')
+
+            if user.role == 'admin':
+                return redirect('admin_dashboard')
+            else:
+                return redirect('home')
         else:
             messages.error(request, "There was an error logging in, try again...")
-            return render(request, 'login_page.html')
-    else:
-        return render(request, "login_page.html")
+    return render(request, 'login_view.html')
+
+def is_admin(user):
+    return user.is_authenticated and user.role=="admin"
+
+def is_passeneger(user):
+    return user.is_authenticated and user.role=="passenger"
+
+@login_required
+@user_passes_test(is_admin)
+def admin_dashboard(request):
+    return render(request, "admin_dashboard.html")
+
+@login_required
+@user_passes_test(is_passeneger)
+def home_page(request):
+    return render(request, "home_page.html")
+
